3a9d7a2c2eefd945947d28536fc70fa950e02f0e
Vedant <vedant.khandelwal@falkonry.com>
2020-03-06 16:37:42 +0530

    remove save function

--- handler.js
+++ handler.js
@@ -5,22 +5,45 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 
 const s3 = new AWS.S3();
 
-module.exports.save = (event, context, callback) => {
-  fetch(event.image_url)
-    .then((response) => {
-      if (response.ok) {
-        return response;
+const request = require('request');
+const async = require('async');
+
+module.exports.import = (event, context, callback) => {
+  
+  var count = 1;
+  var MIN = 1;
+  var MAX = 100;
+  function getRandom() {
+    return Math.random() * (MAX - MIN) + MIN;
+  }
+  var URL = "https://caif9vt4h5.execute-api.us-east-1.amazonaws.com/test/events";
+  async.whilst(function (cb) {
+    return cb(null, count <= 100000);
+  },
+  function (next) {
+    var obj = {
+      time : new Date().toISOString(),
+      entity : "e1",
+      value : getRandom(),
+      tenant : "tenant1",
+      datastream : "datastream1",
+      signal: "signal1"
+    }
+    console.log(JSON.stringify(obj));
+    request({ url : URL, method : "POST", json : obj}, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body)
+      } else {
+        console.error(err);
       }
-      return Promise.reject(new Error(
-            `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
-    })
-    .then(response => response.buffer())
-    .then(buffer => (
-      s3.putObject({
-        Bucket: process.env.BUCKET,
-        Key: event.key,
-        Body: buffer,
-      }).promise()
-    ))
-    .then(v => callback(null, v), callback);
+      count++;
+      setTimeout(function(){
+        next();
+      }, 10);  
+    });
+  },
+  function (err) {
+    console.log("done", err);
+    // All things are done!
+  });
 };
